bb286a7f495e762babc5dc881067096945b25a00
kavana77 <kavanab77@gmail.com>
2026-01-06 16:52:03 +0530

    refactor firebase auth to env-based credentials

--- server/src/config/firebaseConfig.ts
+++ server/src/config/firebaseConfig.ts
@@ -1,11 +1,13 @@
 import admin from 'firebase-admin'
 import env from '../utils/validation'
 
-const serviceAccount = JSON.parse(
- env.FIREBASE_SERVICE_ACCOUNT as string
-);
+
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    credential: admin.credential.cert({
+    projectId: env.FIREBASE_PROJECT_ID,
+    clientEmail: env.FIREBASE_CLIENT_EMAIL,
+    privateKey: env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
     databaseURL: env.FIREBASE_DATABASE_URL
 })
 
--- server/src/utils/validation.ts
+++ server/src/utils/validation.ts
@@ -1,12 +1,15 @@
 import "dotenv/config"
 import { cleanEnv } from "envalid"
 import { port ,str} from "envalid/dist/validators"
+import { FIREBASE_CONFIG_VAR } from "firebase-admin/lib/app/lifecycle"
 
 const env = cleanEnv(process.env, {
     PORT: port(),
     FIREBASE_DATABASE_URL: str(),
     EMAIL_USER: str(),
     EMAIL_PASS: str(),
-    FIREBASE_SERVICE_ACCOUNT: str()
+    FIREBASE_PROJECT_ID: str(),
+    FIREBASE_CLIENT_EMAIL: str(),
+    FIREBASE_PRIVATE_KEY: str(),
 })
 export default env
